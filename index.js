5baa2bf80adf04ce0ecef02a6ad8563d747d3756
YUNH7 <yunny126@gmail.com>
2023-04-09 23:43:43 +0900

    주석 삭제

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
-console.log('hi')
+const addToDoButton = document.querySelector("button");
 
-const addToDoButton = document.querySelector("button")
+addToDoButton.addEventListener("click", (e) => {
+  // 리스트 만들기
+  const newListElement = document.createElement("li");
+  const newToDoSpan = document.createElement("span");
+  const newInputElement = document.createElement("input");
+  newInputElement.type = "checkbox";
+  // input text value 가져오기
+  const addToDoInput = document.querySelector("#saveToDoSomething");
+  newToDoSpan.textContent = addToDoInput.value;
+  newToDoSpan.prepend(newInputElement);
+  newListElement.append(newToDoSpan);
 
-// addToDoInput.addEventListener('keyup', (e) => {
-//     addToDoInput.value = e.target.value
-// })
+  const newDateSpan = document.createElement("span");
+  newDateSpan.className = "addDate";
+  newDateSpan.textContent = new Date().toLocaleString();
 
-addToDoButton.addEventListener('click', (e) => {
-    // 리스트 만들기
-    const newListElement = document.createElement('li')
-    const newToDoSpan = document.createElement('span')
-    const newInputElement = document.createElement('input')
-    newInputElement.type = 'checkbox'
-    // input text value 가져오기
-    const addToDoInput = document.querySelector("#saveToDoSomething")
-    newToDoSpan.textContent = addToDoInput.value
-    newToDoSpan.prepend(newInputElement)
-    newListElement.append(newToDoSpan)
+  const newDeleteSpan = document.createElement("span");
+  newDeleteSpan.className = "deleteToDo";
+  newDeleteSpan.textContent = "X";
 
-    const newDateSpan = document.createElement('span')
-    newDateSpan.className = "addDate"
-    newDateSpan.textContent = new Date().toLocaleString()
+  newDateSpan.append(newDeleteSpan);
+  newListElement.append(newDateSpan);
 
-    const newDeleteSpan = document.createElement('span')
-    newDeleteSpan.className = 'deleteToDo'
-    newDeleteSpan.textContent = "X"
-    
-    newDateSpan.append(newDeleteSpan)
-    newListElement.append(newDateSpan)
-    
-    // ul에 붙이기
-    const toDoListWrapper = document.querySelector('.toDoListWrapper')
-    toDoListWrapper.prepend(newListElement)
-    e.preventDefault()
-
-})
+  // ul에 붙이기
+  const toDoListWrapper = document.querySelector(".toDoListWrapper");
+  toDoListWrapper.prepend(newListElement);
+  e.preventDefault();
+});
